898fd449d756bb2d6e9d61ec94f929e263a91786
Wes Hargrove <wesleyhargrove@gmail.com>
2023-12-02 13:04:41 -0700

    Add Day2

---
+++ src/day2.rs
@@ -0,0 +1,13 @@
+use crate::Day;
+
+pub struct Day2;
+
+impl Day for Day2 {
+    fn command() -> clap::Command {
+        todo!()
+    }
+
+    fn run(_matches: &clap::ArgMatches) {
+        todo!()
+    }
+}
